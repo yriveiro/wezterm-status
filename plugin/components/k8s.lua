66683d70c498d6848b7b0c65492e2f5b2990e07a
Yago Riveiro <yago.riveiro@gmail.com>
2025-01-01 16:00:44 +0000

    fix: kubectl exists func
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- plugin/components/k8s.lua
+++ plugin/components/k8s.lua
@@ -4,7 +4,7 @@ local nerdfonts = wezterm.nerdfonts
 local M = {}
 
 --- Checks if kubectl command-line tool exists in system PATH
----@return integer Exit code (0 if kubectl exists, non-zero if it doesn't)
+---@return boolean true if kubectl exists, false if it doesn't
 function M.kubectl_exists()
   -- Early check for Windows vs Unix-like systems
   local isWindows = package.config:sub(1, 1) == '\\'
@@ -15,7 +15,7 @@ function M.kubectl_exists()
 
   if not handle then
     -- If we couldn't even execute the command, return error
-    return 1
+    return false
   end
 
   -- Read output and close handle
@@ -24,7 +24,7 @@ function M.kubectl_exists()
 
   -- Ensure we return an integer (0 for success, 1 for failure)
   -- If code is nil or non-zero, return 1
-  return code == 0 and 0 or 1
+  return code == 0 and true or false
 end
 
 --- Gets the current kubectl context
